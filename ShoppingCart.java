1209ecb02fec13794a1bd034e0cf858f538cd647
vaishnaviramngoal <amngoalv@gmail.com>
2024-10-10 18:11:27 +0530

    class and object

---
+++ ShoppingCart.java
@@ -0,0 +1,154 @@
+public class ShoppingCart {
+
+    public static void main (String[] args){
+   
+    Shopping shopping = new Shopping();
+    shopping.Items = 2;
+    shopping.TotalPrice = 356.99;
+    shopping.Discount = 5.0;
+    shopping.UserId="vaishnavi1";
+    shopping.isEmptyCart=true;
+
+    System.out.println("add item into the cart :" + shopping.Items);
+    System.out.println("total price of the product:" + shopping.TotalPrice);
+    System.out.println("discount for product :" + shopping.Discount);
+    System.out.println("user id for the customer :" + shopping.UserId);
+    System.out.println("the cart is empty :" + shopping.isEmptyCart);
+
+    System.out.println("------------------------------------");
+
+    Shopping shopping1 = new Shopping();
+    shopping1.Items = 25;
+    shopping1.TotalPrice = 400.99;
+    shopping1.Discount = 20.0;
+    shopping1.UserId="kavya21";
+    shopping1.isEmptyCart=false;
+ 
+    System.out.println("add item into the cart :" + shopping1.Items);
+    System.out.println("total price of the product:" + shopping1.TotalPrice);
+    System.out.println("discount for product :" + shopping1.Discount);
+    System.out.println("user id for the customer :" + shopping1.UserId);
+    System.out.println("the cart is empty :" + shopping1.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+
+    Shopping shopping2 = new Shopping();
+    shopping2.Items = 10;
+    shopping2.TotalPrice = 1189.99;
+    shopping2.Discount = 50.0;
+    shopping2.UserId="sahana67";
+    shopping2.isEmptyCart=true;
+
+    System.out.println("add item into the cart :" + shopping2.Items);
+    System.out.println("total price of the product:" + shopping2.TotalPrice);
+    System.out.println("discount for product :" + shopping2.Discount);
+    System.out.println("user id for the customer :" + shopping2.UserId);
+    System.out.println("the cart is empty :" + shopping2.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+  
+    Shopping shopping3 = new Shopping();
+    shopping3.Items = 25;
+    shopping3.TotalPrice = 1500.99;
+    shopping3.Discount = 15.0;
+    shopping3.UserId="sujata93";
+    shopping3.isEmptyCart=false;
+
+    System.out.println("add item into the cart :" + shopping3.Items);
+    System.out.println("total price of the product:" + shopping3.TotalPrice);
+    System.out.println("discount for product :" + shopping3.Discount);
+    System.out.println("user id for the customer :" + shopping3.UserId);
+    System.out.println("the cart is empty :" + shopping3.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+   
+    Shopping shopping4 = new Shopping();
+    shopping4.Items = 50;
+    shopping4.TotalPrice = 2000.99;
+    shopping4.Discount = 5.0;
+    shopping4.UserId="usha31";
+    shopping4.isEmptyCart=true;
+
+    System.out.println("add item into the cart :" + shopping4.Items);
+    System.out.println("total price of the product:" + shopping4.TotalPrice);
+    System.out.println("discount for product :" + shopping4.Discount);
+    System.out.println("user id for the customer :" + shopping4.UserId);
+    System.out.println("the cart is empty :" + shopping4.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+   
+    Shopping shopping5 = new Shopping();
+    shopping5.Items = 27;
+    shopping5.TotalPrice = 1500.99;
+    shopping5.Discount = 10.0;
+    shopping5.UserId="keerthi123";
+    shopping5.isEmptyCart=false;
+
+    System.out.println("add item into the cart :" + shopping5.Items);
+    System.out.println("total price of the product:" + shopping5.TotalPrice);
+    System.out.println("discount for product :" + shopping5.Discount);
+    System.out.println("user id for the customer :" + shopping5.UserId);
+    System.out.println("the cart is empty :" + shopping5.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+   
+    Shopping shopping6 = new Shopping();
+    shopping6.Items = 35;
+    shopping6.TotalPrice = 4500.99;
+    shopping6.Discount = 20.0;
+    shopping6.UserId="ambika08";
+    shopping6.isEmptyCart=true;
+
+    System.out.println("add item into the cart :" + shopping6.Items);
+    System.out.println("total price of the product:" + shopping6.TotalPrice);
+    System.out.println("discount for product :" + shopping6.Discount);
+    System.out.println("user id for the customer :" + shopping6.UserId);
+    System.out.println("the cart is empty :" + shopping6.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+    Shopping shopping7 = new Shopping();
+    shopping7.Items = 5;
+    shopping7.TotalPrice = 540.99;
+    shopping7.Discount = 40.0;
+    shopping7.UserId="pooja1234";
+    shopping7.isEmptyCart=false;
+
+    System.out.println("add item into the cart :" + shopping7.Items);
+    System.out.println("total price of the product:" + shopping7.TotalPrice);
+    System.out.println("discount for product :" + shopping7.Discount);
+    System.out.println("user id for the customer :" + shopping7.UserId);
+    System.out.println("the cart is empty :" + shopping7.isEmptyCart);
+    
+    System.out.println("------------------------------------");
+    Shopping shopping8 = new Shopping();
+    shopping8.Items = 2;
+    shopping8.TotalPrice = 150.99;
+    shopping8.Discount = 15.0;
+    shopping8.UserId="soujanya123";
+    shopping8.isEmptyCart=true;
+
+    System.out.println("add item into the cart :" + shopping8.Items);
+    System.out.println("total price of the product:" + shopping8.TotalPrice);
+    System.out.println("discount for product :" + shopping8.Discount);
+    System.out.println("user id for the customer :" + shopping8.UserId);
+    System.out.println("the cart is empty :" + shopping8.isEmptyCart);
+    
+    System.out.println("------------------------------------"); 
+
+    Shopping shopping9 = new Shopping();
+    shopping9.Items = 25;
+    shopping9.TotalPrice = 5000.99;
+    shopping9.Discount = 50.0;
+    shopping9.UserId="prabhu017";
+    shopping9.isEmptyCart=false;
+
+    System.out.println("add item into the cart :" + shopping9.Items);
+    System.out.println("total price of the product:" + shopping9.TotalPrice);
+    System.out.println("discount for product :" + shopping9.Discount);
+    System.out.println("user id for the customer :" + shopping9.UserId);
+    System.out.println("the cart is empty :" + shopping9.isEmptyCart);
+    
+
+
+   }
+}
