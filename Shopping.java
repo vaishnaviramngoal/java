06b0515629d605432a72c9497ee24ad05d4e8c2f
vaishnaviramngoal <amngoalv@gmail.com>
2024-10-10 18:08:10 +0530

    class and object

---
+++ Shopping.java
@@ -0,0 +1,23 @@
+public class Shopping {
+
+   
+    int Items;
+    double TotalPrice;
+    double Discount;
+    String UserId;
+    boolean isEmptyCart;
+   
+    
+   public static void addItem(){
+    
+   }
+   public static void removeItem(){
+    
+   }
+   public static void clearCart(){
+   
+    }
+   public static void viewItems(){
+   
+   }
+}
