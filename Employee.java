f74bb0d153637b1a0a2f8b2b7fb71c89d9609162
vaishnaviramngoal <amngoalv@gmail.com>
2024-10-16 23:58:08 +0530

    Using array

---
+++ Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+    int id;
+    String name;
+    double salary;
+    String employmentType;
+
+    
+    public Employee(int id, String name, double salary, String employmentType) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.employmentType = employmentType;
+    }
+
+    
+    public void EmployeeInfo() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+        System.out.println("Employment Type: " + employmentType);
+        
+    }
+}
+
+
