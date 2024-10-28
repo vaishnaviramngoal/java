a3b92f32ed53ab1afe328d8a948d3cfb57b6bdb6
vaishnaviramngoal <amngoalv@gmail.com>
2024-10-28 14:29:08 +0530

    constructor chaining

---
+++ Student1.java
@@ -0,0 +1,13 @@
+public class Student1 {
+
+      public static void main(String[] args) {
+
+
+
+       Students students = new Students("Keerthi", 84, "Electronics", 23, "Banglore");
+       students.StudentsInfo();
+
+
+
+       }
+}
